3cc5fe5431a8ba64826d95f46db74a88134b4316
iamYadikar <145845214+iamYadikar@users.noreply.github.com>
2023-10-14 18:26:00 +0600

    Update yadi.py код бота дискорд

--- yadi.py
+++ yadi.py
@@ -1,18 +1,42 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "ЖИЗА": "Жизненная ситуация",
-            "РОФЛ": "Шутка",
-            "СОРЯН": "Прости",
-            "КРАШ": "Человек который нравится другому человеку",
-            "ЧЕЛ": "Человек"}
-            
-word = input("Введите непонятное слово (большими буквами!): ")
+import discord
+import random
+from discord.ext import commands
 
-if word in meme_dict.keys():
-    # Что делать, если слово нашлось?
-    print(meme_dict[word])
-else:
-    # Что делать, если слово не нашлось?
-    print('Такого слова я не знаю')
+intents = discord.Intents.default() # Переменная intents - хранит привилегии бота
+intents.message_content = True # Включаем привелегию на чтение сообщений
+
+
+bot = commands.Bot(command_prefix='$', intents=intents) # Создаем бота в переменной client и передаем все привелегии
+
+
+def generator_pass(pass_length = 10):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(generator_pass())
+
+bot.run('token')
     
